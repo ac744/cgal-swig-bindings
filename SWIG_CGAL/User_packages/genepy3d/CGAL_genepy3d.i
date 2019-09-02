a55af2a1d1b8b6ec9adc195583d8f6abf7aa5a9f
ac744 <anatole.chessel@polytechnique.edu>
2019-09-02 14:54:29 +0200

    triangle output for as

--- SWIG_CGAL/User_packages/genepy3d/CGAL_genepy3d.i
+++ SWIG_CGAL/User_packages/genepy3d/CGAL_genepy3d.i
@@ -6,19 +6,25 @@ Decl_void_type()
 %import  "SWIG_CGAL/Common/Macros.h"
 %import  "SWIG_CGAL/Kernel/CGAL_Kernel.i"
 %include "SWIG_CGAL/Common/Iterator.h"
-
+%include  "SWIG_CGAL/Common/Output_iterator_wrapper.h"
 %import "SWIG_CGAL/Polyhedron_3/CGAL_Polyhedron_3.i"
 %{
         #include <SWIG_CGAL/Polyhedron_3/all_includes.h>
         #include <SWIG_CGAL/Kernel/Point_3.h>
+        #include <SWIG_CGAL/Common/Output_iterator_wrapper.h>
 %}
 SWIG_CGAL_import_Polyhedron_3_SWIG_wrapper
 
 %types(Point_3*,Point_3);//needed so that the identifier SWIGTYPE_p_Point_3 is generated
 SWIG_CGAL_set_wrapper_iterator_helper_input(Point_3)
-
-//  typemaps for output iterator of Point_3
 SWIG_CGAL_set_wrapper_iterator_helper_output(Point_3)
+
+%types(Triangle_3*,Triangle_3);//needed so that the identifier SWIGTYPE_p_Point_3 is generated
+SWIG_CGAL_set_wrapper_iterator_helper_input(Triangle_3)
+SWIG_CGAL_set_wrapper_iterator_helper_output(Triangle_3)
+
+%define Integer_output_iterator boost::function_output_iterator<Container_writer<int,int> > %enddef
+SWIG_CGAL_output_iterator_typemap_in(Integer_output_iterator,int,Integer,int,swig_types[0],"Ljava/lang/Integer;")
 
 
 %include "SWIG_CGAL/User_packages/genepy3d/myAlphaShape3.h" //this one is for SWIG
@@ -26,12 +32,4 @@ SWIG_CGAL_set_wrapper_iterator_helper_output(Point_3)
 #include <SWIG_CGAL/User_packages/genepy3d/myAlphaShape3.h> //this one is for the C++ wrapper
 %}
  
-//%inline 
-// void modify_polyhedron(Polyhedron_3_SWIG_wrapper& polyhedron){
-//%{
-//  polyhedron.make_tetrahedron(Point_3::cpp_base(0,0,0),
-//	                                Point_3::cpp_base(0,0,1),
-//	                                Point_3::cpp_base(0,1,0),
-//	                                Point_3::cpp_base(1,1,1));
-// }
-//%}
+
--- SWIG_CGAL/User_packages/genepy3d/myAlphaShape3.h
+++ SWIG_CGAL/User_packages/genepy3d/myAlphaShape3.h
@@ -1,6 +1,7 @@
 #include <SWIG_CGAL/Common/Wrapper_iterator_helper.h>
 #include <SWIG_CGAL/Kernel/Point_3.h>
 #include <SWIG_CGAL/Kernel/Vector_3.h>
+#include <SWIG_CGAL/Kernel/Triangle_3.h>
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Alpha_shape_3.h>
@@ -17,10 +18,11 @@ typedef CGAL::Triangulation_data_structure_3<Vb,Fb>      Tds;
 typedef CGAL::Delaunay_triangulation_3<K,Tds,CGAL::Fast_location>  Delaunay;
 typedef CGAL::Alpha_shape_3<Delaunay>                    Alpha_shape_3;
 typedef K::Point_3                                       Point;
+typedef K::Triangle_3                                       Triangle;
 typedef Alpha_shape_3::Alpha_iterator                    Alpha_iterator;
 typedef Alpha_shape_3::NT                                NT;
 
-void getAlphaShape(Wrapper_iterator_helper<Point_3>::input point_range,Wrapper_iterator_helper<Point_3>::output pointsOut ){
+void getAlphaShape(Wrapper_iterator_helper<Point_3>::input point_range, Wrapper_iterator_helper<Point_3>::output pointsOut,Wrapper_iterator_helper<Triangle_3>::output triOut){
   typedef Wrapper_iterator_helper<Point_3>::input::first_type Iterator;
 
   Delaunay dt;
@@ -34,12 +36,23 @@ void getAlphaShape(Wrapper_iterator_helper<Point_3>::input point_range,Wrapper_i
   as.set_alpha(*opt);
   assert(as.number_of_solid_components() == 1);
 
-  std::list<Alpha_shape_3::Vertex_handle> outlist;
+  //Output of coordinates of vertex in the alpha shape
+  std::list<Alpha_shape_3::Vertex_handle> outPointsList;
   std::list<Alpha_shape_3::Vertex_handle>::iterator it2;
-  as.get_alpha_shape_vertices(std::back_inserter(outlist),Alpha_shape_3::REGULAR,*opt);
+  as.get_alpha_shape_vertices(std::back_inserter(outPointsList),Alpha_shape_3::REGULAR,*opt);
 
-  for (it2=outlist.begin();it2!=outlist.end();it2++){
+  for (it2=outPointsList.begin();it2!=outPointsList.end();it2++){
         *pointsOut++=(*it2)->point();
+  }
+
+  //Output of the triangles
+  std::list<Alpha_shape_3::Facet> outFacetsList;
+  std::list<Alpha_shape_3::Facet>::iterator it3;
+  as.get_alpha_shape_facets(std::back_inserter(outFacetsList),Alpha_shape_3::REGULAR,*opt);
+  
+  for (it3=outFacetsList.begin();it3!=outFacetsList.end();it3++){
+        *triOut++=dt.triangle(*it3);
+
   }
 
 }
